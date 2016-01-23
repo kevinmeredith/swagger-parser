6fc1dc2fea48a9f981e6c99440413093e9a880b0
Ole Lensmar <ole.lensmar@smartbear.com>
2016-01-23 15:17:11 -0600

    introduced httpclient for fetching swagger definitions - with lax:ed SSL support

--- modules/swagger-parser/src/main/java/io/swagger/parser/util/RemoteUrl.java
+++ modules/swagger-parser/src/main/java/io/swagger/parser/util/RemoteUrl.java
@@ -1,141 +1,101 @@
 package io.swagger.parser.util;
 
 import io.swagger.models.auth.AuthorizationValue;
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.net.URI;
 import java.net.URLEncoder;
 import java.security.KeyManagementException;
+import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.util.List;
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSession;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
 
 public class RemoteUrl {
 
     private static final String ACCEPT_HEADER_VALUE = "application/json, application/yaml, */*";
+    private static CloseableHttpClient httpClient;
 
-    private static void disableSslVerification() {
-        try {
-            // Create a trust manager that does not validate certificate chains
-            TrustManager[] trustAllCerts = new TrustManager[]{
-                new X509TrustManager() {
-                    public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-                        return null;
+    private static synchronized CloseableHttpClient getCarelessHttpClient() {
+
+        if (httpClient == null) {
+            try {
+                SSLContextBuilder builder = new SSLContextBuilder();
+                builder.loadTrustMaterial(null, new TrustStrategy() {
+                    public boolean isTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+                        return true;
                     }
-
-                    public void checkClientTrusted(X509Certificate[] certs, String authType) {
-                    }
-
-                    public void checkServerTrusted(X509Certificate[] certs, String authType) {
-                    }
-                }
-            };
-
-            // Install the all-trusting trust manager
-            SSLContext sc = SSLContext.getInstance("SSL");
-            sc.init(null, trustAllCerts, new java.security.SecureRandom());
-            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-
-            // Create all-trusting host name verifier
-            HostnameVerifier allHostsValid = new HostnameVerifier() {
-                public boolean verify(String hostname, SSLSession session) {
-                    return true;
-                }
-            };
-
-            // Install the all-trusting host verifier
-            HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (KeyManagementException e) {
-            e.printStackTrace();
+                });
+                SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(builder.build(), NoopHostnameVerifier.INSTANCE);
+                httpClient = HttpClients
+                    .custom()
+                    .setSSLSocketFactory(sslsf)
+                    .build();
+            } catch (NoSuchAlgorithmException | KeyStoreException | KeyManagementException e) {
+                System.err.println("can't disable SSL verification;" + e);
+            }
         }
+
+        return httpClient;
     }
 
     public static String urlToString(String url, List<AuthorizationValue> auths) throws Exception {
-        InputStream is = null;
-        URLConnection conn = null;
-        BufferedReader br = null;
 
-        try {
-            if (auths != null) {
-                StringBuilder queryString = new StringBuilder();
-                // build a new url if needed
-                for (AuthorizationValue auth : auths) {
-                    if ("query".equals(auth.getType())) {
-                        if (queryString.toString().length() == 0) {
-                            queryString.append("?");
-                        } else {
-                            queryString.append("&");
-                        }
-                        queryString.append(URLEncoder.encode(auth.getKeyName(), "UTF-8"))
-                                .append("=")
-                                .append(URLEncoder.encode(auth.getValue(), "UTF-8"));
+        HttpGet getMethod = new HttpGet(url);
+        getMethod.setHeader("Accept", ACCEPT_HEADER_VALUE);
+
+        if (auths != null) {
+            StringBuilder queryString = new StringBuilder();
+            // build a new url if needed
+            for (AuthorizationValue auth : auths) {
+                if ("query".equals(auth.getType())) {
+                    if (queryString.toString().length() == 0) {
+                        queryString.append("?");
+                    } else {
+                        queryString.append("&");
                     }
-                }
-                if (queryString.toString().length() != 0) {
-                    url = url + queryString.toString();
-                }
-                conn = new URL(url).openConnection();
-
-                for (AuthorizationValue auth : auths) {
-                    if ("header".equals(auth.getType())) {
-                        conn.setRequestProperty(auth.getKeyName(), auth.getValue());
-                    }
-                }
-            } else {
-                conn = new URL(url).openConnection();
-            }
-
-            conn.setRequestProperty("Accept", ACCEPT_HEADER_VALUE);
-            conn.connect();
-            InputStream in = conn.getInputStream();
-
-            StringBuilder contents = new StringBuilder();
-
-            BufferedReader input = new BufferedReader(
-                    new InputStreamReader(in, "UTF-8"));
-
-            for (int i = 0; i != -1; i = input.read()) {
-                char c = (char) i;
-                if (!Character.isISOControl(c)) {
-                    contents.append((char) i);
-                }
-                if (c == '\n') {
-                    contents.append('\n');
+                    queryString.append(URLEncoder.encode(auth.getKeyName(), "UTF-8"))
+                        .append("=")
+                        .append(URLEncoder.encode(auth.getValue(), "UTF-8"));
                 }
             }
-
-            in.close();
-
-            return contents.toString();
-        } catch (javax.net.ssl.SSLProtocolException e) {
-            System.out.println("there is a problem with the target SSL certificate");
-            System.out.println("**** you may want to run with -Djsse.enableSNIExtension=false\n\n");
-            e.printStackTrace();
-            throw e;
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw e;
-        } finally {
-            if (is != null) {
-                is.close();
+            if (queryString.toString().length() != 0) {
+                url = url + queryString.toString();
             }
-            if (br != null) {
-                br.close();
+
+            getMethod.setURI(URI.create(url));
+            for (AuthorizationValue auth : auths) {
+                if ("header".equals(auth.getType())) {
+                    getMethod.setHeader(auth.getKeyName(), auth.getValue());
+                }
             }
         }
-    }
 
-    static {
-        disableSslVerification();
+        final CloseableHttpClient httpClient = getCarelessHttpClient();
+
+        if (httpClient != null) {
+            final CloseableHttpResponse response = httpClient.execute(getMethod);
+
+            try {
+                HttpEntity entity = response.getEntity();
+                return EntityUtils.toString(entity, "UTF-8");
+            } finally {
+                response.close();
+                httpClient.close();
+            }
+        } else {
+            throw new IOException("CloseableHttpClient could not be initialized");
+        }
     }
 }
